7aebac5e84ff88eab53338d270dd701b2dbc385c
Jahlapeno1 <jimenezaot@gmail.com>
2024-03-28 15:33:36 -0400

    stash

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const multiplesOfThree = () => {
 
 const multiplesOfThreeOrFive = () => {
     for (let i = 1; i <= 100; i++)
-        if (i % 3 === 0 || i % 5 === 0 ) {
+        if (i % 3 === 0 || i % 5 === 0) {
             console.log(i)
         }
 }
